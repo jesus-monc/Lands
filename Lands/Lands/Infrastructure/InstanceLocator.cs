74f7ce7c1ca3f89bf3afcd79c19f9d687440cef4
m.c.jesus90 <m.c.jesus90@gmail.com>
2019-12-27 13:38:56 -0700

    Formulario de Login

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,16 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+    }
+}
